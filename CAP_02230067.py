ceb17190b5bc7cd65921dcadf8b8097f3d958499
Gyeltshen369 <02230067.cst@rub.edu.bt>
2024-04-12 23:09:36 +0530

    Changes to CAP_02230067.py

--- CAP_02230067.py
+++ CAP_02230067.py
@@ -20,7 +20,7 @@ def read_input():
     return file
 
 def calculation_score(point):
-    score={'A X': 2, 'A Y': 4, 'A Z': 9, 'B X': 1, 'B Y': 5, 'B Z': 7, 'C X': 1, 'C Y': 6, 'C Z': 7} # The dictionary contains the outcome keys and the corresponding values 
+    score={'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7} # The dictionary contains the outcome keys and the corresponding values 
     total_score=0
     for line in point:
         each=line.strip() # It reads the line in the input file
